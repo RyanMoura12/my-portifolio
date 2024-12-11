3624392c3c9ca098b1f52a75f76011c4b3b9bd62
Moura Ryan <ryan92victor@gmail.com>
2024-12-11 02:24:17 -0300

    new commit

--- src/projectsdata.tsx
+++ src/projectsdata.tsx
@@ -10,7 +10,7 @@ const ProjectsData = [
     title: "Sicarp",
     description:
       "Through SICARP, it is possible to monitor and audit information related to information, ensuring transparency and control over public purchases. Furthermore, it allows greater agility and efficiency in the management of these processes, avoiding rework and errors.",
-    tool: "React · Firebase",
+    tool: "Vue.js · Laravel - Graphql",
     github: "https://github.com/ogabrielbarbosa/TCC-Professor",
     link: "https://sicarp.diretoriodigital.net.br/",
   },
@@ -20,7 +20,7 @@ const ProjectsData = [
     title: "Sipas",
     description:
       "It is a platform for recording the City Hall's main social services such as: TFD – Treatment Outside the Home, Basic Food Basket, Baby Kit, Medicines, etc.",
-    tool: "React · Firebase",
+    tool: "Vue.js · Laravel - Graphql",
     github: "https://github.com/ogabrielbarbosa/TCC-Orientador",
     link: "https://sipas.diretoriodigital.net.br/",
   },
@@ -30,7 +30,7 @@ const ProjectsData = [
     title: "Siato - Atendimento Social",
     description:
       "is a system that controls the bidding process, controlling all stages and generating documents that are used for each stage of the process.",
-    tool: "React · TypeScript · PokeAPI",
+    tool: "Vue.js · Laravel - Graphql",
     github: "https://github.com/ogabrielbarbosa/pokedex",
     link: "https://siato.diretoriodigital.net.br/",
   },
@@ -40,7 +40,7 @@ const ProjectsData = [
     title: "PraLicitar",
     description:
       "Plataforma de filmes em exibição, com acesso a informações do filme, como tempo de duração, gêneros, sinopse, entre outras coisas.",
-    tool: "React · TheMovieDB",
+    tool: "Vue.js · Laravel - GraphqlB",
     github: "https://github.com/ogabrielbarbosa/flix",
     link: "https://pralicitar.diretoriodigital.net.br/",
   },
